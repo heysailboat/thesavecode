32cf4bfa8907580c1865cac7846a29a00a9d9119
Lucian Raileanu <lucian@raileanu.ca>
2026-06-14 14:51:47 -0400

    feat: change key input to slash-separated, make program loop, and add help mode
    slash-separated keys also allow for removal of ast library.

--- basic-slash.py
+++ basic-slash.py
@@ -5,27 +5,30 @@ This generates multiple blocks split with slashes.
 """
 
 import os
-import ast
 
 savecode = None
 
 os.system('clear')
 
-mode = input("Enter mode (read/write): ").lower()
+while True:
+    mode = input("Enter mode (read/write/help): ").lower()
 
-if mode == "write":
-    pass
-elif mode == "read":
-    
-    key_string = input("Enter key as a Python list: ")
-    key = ast.literal_eval(key_string)
-    
-    code = input("Enter save code: ")
-    split_code = code.split("/")
-    
-    if len(split_code) > len(key):
-        print(f"Note: entered code has {len(split_code) - len(key)} more blocks than the key. Only the first {len(key)} blocks will be read.")
-    
-    for k, val in zip(key, split_code):
-        print(f"{k}: {val}\n")
+    if mode == "write":
+        pass
+    elif mode == "read":
         
+        key_string = input("Enter key:")
+        key = key_string.split("/")
+        
+        code = input("Enter save code: ")
+        split_code = code.split("/")
+        
+        if len(split_code) > len(key):
+            print(f"Note: entered code has {len(split_code) - len(key)} more blocks than the key. Only the first {len(key)} blocks will be read.")
+        
+        for k, val in zip(key, split_code):
+            print(f"{k}: {val}\n")
+    elif mode == "help":
+        print("""Help:
+              This program generates and reads slash-separated save codes. 
+              """)

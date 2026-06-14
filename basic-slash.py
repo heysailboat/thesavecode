2154e6ba35c828bef050324789f7fa1a984bc19a
Lucian Raileanu <lucian@raileanu.ca>
2026-06-14 14:56:50 -0400

    feat: move modes to functions

--- basic-slash.py
+++ basic-slash.py
@@ -10,29 +10,34 @@ savecode = None
 
 os.system('clear')
 
+def write():
+    key_string = input("Enter desired key: ")
+    key = key_string.split("/")
+    
+    savecode = "/".join([input(f"Enter value for {k}: ") for k in key])
+    
+    print(f"Save code: {savecode}")
+    
+def read():
+    key_string = input("Enter key: ")
+    key = key_string.split("/")
+        
+    code = input("Enter save code: ")
+    split_code = code.split("/")
+        
+    if len(split_code) > len(key):
+        print(f"Note: entered code has {len(split_code) - len(key)} more blocks than the key. Only the first {len(key)} blocks will be read.")
+        
+    for k, val in zip(key, split_code):
+        print(f"{k}: {val}")
+
 while True:
     mode = input("Enter mode (read/write/help): ").lower()
 
     if mode == "write":
-        key_string = input("Enter desired key: ")
-        key = key_string.split("/")
-        
-        savecode = "/".join([input(f"Enter value for {k}: ") for k in key])
-        
-        print(f"Save code: {savecode}")
+        write()
     elif mode == "read":
-        
-        key_string = input("Enter key: ")
-        key = key_string.split("/")
-        
-        code = input("Enter save code: ")
-        split_code = code.split("/")
-        
-        if len(split_code) > len(key):
-            print(f"Note: entered code has {len(split_code) - len(key)} more blocks than the key. Only the first {len(key)} blocks will be read.")
-        
-        for k, val in zip(key, split_code):
-            print(f"{k}: {val}")
+        read()
     elif mode == "help":
         print("""Help:
               This program generates and reads slash-separated save codes. 
